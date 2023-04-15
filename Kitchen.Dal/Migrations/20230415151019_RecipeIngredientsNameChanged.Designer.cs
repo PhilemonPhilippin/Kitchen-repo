2fdea5b000a0d9e99357bc135df819d5e7f841a6
Philemon Philippin <p_philippin@hotmail.com>
2023-04-15 17:12:13 +0200

    Nouvelle migration pour le nouveau nom de RecipeIngredients

--- Kitchen.Dal/Migrations/20230415151019_RecipeIngredientsNameChanged.Designer.cs
+++ Kitchen.Dal/Migrations/20230415151019_RecipeIngredientsNameChanged.Designer.cs
@@ -4,6 +4,7 @@ using Kitchen.Dal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kitchen.Dal.Migrations
 {
     [DbContext(typeof(KitchenContext))]
-    partial class KitchenContextModelSnapshot : ModelSnapshot
+    [Migration("20230415151019_RecipeIngredientsNameChanged")]
+    partial class RecipeIngredientsNameChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
