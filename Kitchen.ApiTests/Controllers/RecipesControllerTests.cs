beb944c23fec99e9fe1bf5e6cd93a93e396d2bc3
Philemon Philippin <p_philippin@hotmail.com>
2023-07-29 05:40:01 +0200

    Test project for API set up

---
+++ Kitchen.ApiTests/Controllers/RecipesControllerTests.cs
@@ -0,0 +1,26 @@
+﻿
+using Kitchen.Contracts.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Kitchen.Api.Controllers;
+
+public class RecipesControllerTests
+{
+    [Fact]
+    public async Task GetRecipes_Empty_ReturnsNotFound()
+    {
+
+        // Arrange
+        Tools.RecipeService recipeService = new Tools.RecipeService();
+        RecipesController controller = new (new NullLogger<RecipesController>(), recipeService);
+
+        // Act
+        ActionResult<IEnumerable<RecipeDto>> response = await controller.GetRecipes(1, 5, null, null);
+        var result = response.Result;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<NotFoundResult>(result);
+    }
+}
---
+++ Kitchen.ApiTests/Tools/RecipeService.cs
@@ -0,0 +1,39 @@
+﻿using Kitchen.Contracts.Requests;
+using Kitchen.Core.Interfaces;
+using Kitchen.Entities;
+using Kitchen.Models;
+
+namespace Kitchen.Api.Tools;
+
+internal class RecipeService : IRecipeService
+{
+    public Task<DbResult<Recipe>> Add(RecipeRequest createRecipeRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Status> Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DbResult<Recipe>> Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<(IEnumerable<Recipe> recipes, PaginationMetadata metadata)> GetPage(int pageNumber, int pageSize, string? title, string? searchQuery)
+    {
+        return await Task.FromResult((Enumerable.Empty<Recipe>(), new PaginationMetadata(1,5,0)));
+    }
+
+    public Task<bool> IdExist(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Status> Update(int id, RecipeRequest updateRecipeRequest)
+    {
+        throw new NotImplementedException();
+    }
+}
